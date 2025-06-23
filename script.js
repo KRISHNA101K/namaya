6536f34d392026b3276b03d0754a608cc70a3c71
Krishna Goyal <goyalkrishna005@gmail.com>
2025-06-24 03:16:47 +0530

    Update

--- script.js
+++ script.js
@@ -167,4 +167,12 @@ window.searchLeads = async function () {
   }
 };
 
+const sidebarToggle = document.getElementById("sidebar-toggle");
+const sidebar = document.getElementById("sidebar");
+
+if (sidebarToggle && sidebar) {
+  sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+}
 
