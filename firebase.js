04af01e12dfb68f9ebedecda2e7c1916e682a971
Krishna Goyal <goyalkrishna005@gmail.com>
2025-06-24 01:59:42 +0530

    fix

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBmvGglRv-hnPnUuSEB4lWJjO4bUUa7D7s",
   authDomain: "namaya-8665f.firebaseapp.com",
   projectId: "namaya-8665f",
-  storageBucket: "namaya-8665f.firebasestorage.app",
+  storageBucket: "namaya-8665f.appspot.com",
   messagingSenderId: "835556477491",
   appId: "1:835556477491:web:cfeb5bfddb125bb4e3b146"
 };
